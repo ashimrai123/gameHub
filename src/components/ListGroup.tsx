bdacc82c43441ef50883534a16a7fe90407107c7
Ashim Rai <ashrai14691@gmail.com>
2024-02-08 23:54:55 +0545

    listGroup handling events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,24 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const items = ["Action", " Adventure", "RPG", "Strategy", "Shooter"];
+  let items = ["Action", " Adventure", "RPG", "Strategy", "Shooter"];
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>Game Genres List</h1>
+      {items.length === 0 && <p>No items Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className=" list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className=" list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
